d12e636be407bbad283cfe36e78a91022b1eadc2
Tate Sever <gabrieltsever@gmail.com>
2025-12-15 20:09:30 -0500

    Update vite config for cache busting

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,11 @@ export default defineConfig({
       input: {
         main: './index.html',
       },
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
     },
   },
 });
