27a040f6d0273ac94d349585fb9098e0810b36ca
“Tate <gabrieltsever@gmail.com>
2025-12-15 19:13:11 -0500

    UXstudio

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Photography from './pages/Photography';
 import OtherProjects from './pages/OtherProjects';
 import D3Vis from './pages/d3Vis';
 import Quicklinks from './pages/quicklinks';
-import UXstudio from './pages/UXstudio';
+import UXstudio from './pages/UXstudio/UXstudio';
 
 function App() {
   return (
